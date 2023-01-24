9b5dc536a433a66daa94e0ef0ef2fc3359ad6127
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-01-24 16:50:57 +0000

    feat(main): add helm resource and namespace value

--- main.tf
+++ main.tf
@@ -1,2 +1,48 @@
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
+
+resource "kubernetes_namespace" "trivy-system" {
+  metadata {
+    name = "trivy-system"
+
+    labels = {
+      "name"                                           = "trivy-system"
+      "component"                                      = "trivy-system"
+      "cloud-platform.justice.gov.uk/is-production"    = "true"
+      "cloud-platform.justice.gov.uk/environment-name" = "production"
+    }
+
+    annotations = {
+      "cloud-platform.justice.gov.uk/application"   = "trivy-system"
+      "cloud-platform.justice.gov.uk/business-unit" = "Platforms"
+      "cloud-platform.justice.gov.uk/owner"         = "Cloud Platform: platforms@digital.justice.gov.uk"
+      "cloud-platform.justice.gov.uk/source-code"   = "https://github.com/ministryofjustice/cloud-platform-infrastructure"
+      "cloud-platform.justice.gov.uk/slack-channel" = "cloud-platform"
+      "cloud-platform-out-of-hours-alert"           = "true"
+    }
+  }
+}
+
+
+resource "helm_release" "trivy-system" {
+  name       = "trivy-system"
+  namespace  = kubernetes_namespace.trivy-system.id
+  repository = "https://aquasecurity.github.io/helm-charts/"
+  chart      = "trivy-operator"
+  version    = "0.10.1"
+
+  set {
+    name  = "trivy.severity"
+    value = "CRITICAL"
+  }
+
+  set {
+    name  = "trivy.ignoreUnfixed"
+    value = "true"
+  }
+
+  lifecycle {
+    ignore_changes = [keyring]
+  }
+
+}
