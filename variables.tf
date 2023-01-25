09419a5c2c50a28c1e22ac65fab66db955ce562f
sj-williams <steve.williams@digital.justice.gov.uk>
2023-01-25 14:12:04 +0000

    update default severity level var and example

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "severity_list" {
   description = "A single string providing comma separated list of CVE Severity levels to be monitored. Possible values are UNKNOWN, LOW, MEDIUM, HIGH, CRITICAL"
-  default     = "HIGH,CRITICAL"
+  default     = "UNKNOWN,LOW,MEDIUM,HIGH,CRITICAL"
   type        = string
 }
