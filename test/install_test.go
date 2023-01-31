0bc387219aa0e8b85568587dd4aa0bd3e2e53322
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2023-01-31 18:59:31 +0000

    test(module): add module install test to single test function
    this allows us to create and defer in the same function

--- test/install_test.go
+++ test/install_test.go
@@ -7,23 +7,20 @@ import (
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
-var (
-	namespaceName = "trivy-system"
-	options       = k8s.NewKubectlOptions("", "", namespaceName)
-)
-
-func TestMain(t *testing.T) {
+func TestTrivyModuleInstall(t *testing.T) {
+	var (
+		namespaceName = "trivy-system"
+		options       = k8s.NewKubectlOptions("", "", namespaceName)
+	)
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: "./",
 	})
 
-	defer terraform.Destroy(t, terraformOptions)
+	t.Cleanup(func() {
+		terraform.Destroy(t, terraformOptions)
+	})
 
 	terraform.InitAndApply(t, terraformOptions)
-}
-
-func TestServiceCreation(t *testing.T) {
-	t.Parallel()
 
 	svc, err := k8s.RunKubectlAndGetOutputE(t, options, "get", "svc")
 	if err != nil {
@@ -33,10 +30,6 @@ func TestServiceCreation(t *testing.T) {
 	if svc == "" {
 		t.Fatal("Service is not created")
 	}
-}
-
-func TestDeploymentCreation(t *testing.T) {
-	t.Parallel()
 
 	deploy, err := k8s.RunKubectlAndGetOutputE(t, options, "get", "deploy")
 	if err != nil {
@@ -46,10 +39,6 @@ func TestDeploymentCreation(t *testing.T) {
 	if deploy == "" {
 		t.Fatal("Deployment is not created")
 	}
-}
-
-func TestNamespaceCreation(t *testing.T) {
-	t.Parallel()
 
 	ns, err := k8s.GetNamespaceE(t, options, namespaceName)
 	if err != nil {
